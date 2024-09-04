6f58ca81dcf257d0e2f1338e3be481e420daeda3
gbopola <opeafolabi5@gmail.com>
2024-09-04 21:23:59 +0100

    update billing v2

--- app/(dashboard)/dashboard/billing/page.tsx
+++ app/(dashboard)/dashboard/billing/page.tsx
@@ -29,28 +29,32 @@ const BillingPage = () => {
     //   </div>
     // </div>
     <div>
-      <h1 className="text-3xl font-semibold leading-tight tracking-tight text-gray-900">
-        Billing
-      </h1>
-      <p className="text-gray-600 mt-1 mb-6">Manage your billing plans here.</p>
-      <div className="overflow-hidden rounded-lg bg-white shadow">
+      <div className="mb-8">
+        <h1 className="text-3xl font-semibold leading-tight tracking-tight text-gray-900">
+          Billing
+        </h1>
+        <p className="text-gray-600">Manage your billing plans here.</p>
+      </div>
+      <div className="bg-white border sm:rounded-lg">
         <div className="px-4 py-5 sm:p-6">
-          <h2 className="font-semibold text-gray-900">Your Plan</h2>
-          <p className="text-gray-600 mb-4">
-            You are currently on the Hobby plan.
-          </p>
-          <p className="font-medium text-xl text-gray-900">$10/month</p>
-        </div>
-        <div className="bg-gray-50 px-4 py-4 sm:px-6">
-          <div className="flex justify-between items-center">
+          <h3 className="text-base font-semibold leading-6 text-gray-900">
+            Manage subscription
+          </h3>
+          <div className="mt-2 max-w-xl text-sm text-gray-500">
+            <p>
+              You are currently on the{' '}
+              <span className="font-semibold">Hobby</span> plan.
+            </p>
+          </div>
+          <div className="flex items-center justify-between mt-5">
             <button
               type="button"
-              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
             >
-              Change Plan
+              Change plan
             </button>
-            <p className="text-sm text-gray-600">
-              Your plan renews of January 8, 2024.
+            <p className="mt-2 text-sm text-gray-500">
+              Your plan renews on January 8, 2024.
             </p>
           </div>
         </div>
