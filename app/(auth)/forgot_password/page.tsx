7829988897fc5921b7eb5755e846e1b8e99311c3
gbopola <opeafolabi5@gmail.com>
2024-09-16 15:20:49 +0100

    search param cleanup

--- app/(auth)/forgot_password/page.tsx
+++ app/(auth)/forgot_password/page.tsx
@@ -1,7 +1,11 @@
 import ForgotPassword from '@/components/ui/AuthForms/ForgotPassword';
 import Container from '@/components/ui/Container';
 
-export default function ForgotPasswordPage() {
+export default function ForgotPasswordPage({
+  searchParams
+}: {
+  disable_button: boolean;
+}) {
   return (
     <Container>
       <ForgotPassword redirectMethod="client" />
--- components/ui/AuthForms/ForgotPassword.tsx
+++ components/ui/AuthForms/ForgotPassword.tsx
@@ -11,22 +11,23 @@ import Input from '../Input';
 import { HiExclamationCircle } from 'react-icons/hi2';
 import { forgotPasswordSchema } from '@/validations/auth';
 import { Logo } from '@/components/Shared';
-import { Suspense } from 'react';
 
 // Define prop type with allowEmail boolean
 interface ForgotPasswordProps {
   redirectMethod: string;
+  searchParams: string;
 }
 
 type FormFields = z.infer<typeof forgotPasswordSchema>;
 
 export default function ForgotPassword({
-  redirectMethod
+  redirectMethod,
+  searchParams
 }: ForgotPasswordProps) {
   const router = redirectMethod === 'client' ? useRouter() : null;
 
   //    get search params from the URL
-  const searchParams = useSearchParams().get('disable_button') ? true : false;
+  const params = searchParams === 'disable_button' ? true : false;
 
   const {
     register,
@@ -41,66 +42,64 @@ export default function ForgotPassword({
   };
 
   return (
-     
-      <Suspense>
-        <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <Logo height={10} />
-          <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Reset your password
-          </h2>
-        </div>
+    <>
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <Logo height={10} />
+        <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Reset your password
+        </h2>
+      </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
-          <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
-            <form
-              noValidate={true}
-              className="space-y-6"
-              onSubmit={handleSubmit(onSubmit)}
-            >
-              <div>
-                <Label htmlFor="email">Email</Label>
-                <div className="relative mt-2 rounded-md shadow-sm">
-                  <Input
-                    {...register('email')}
-                    id="email"
-                    name="email"
-                    type="email"
-                    placeholder="you@example.com"
-                    variant={errors.email && 'error'}
-                  />
-                  {errors.email && (
-                    <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
-                      <HiExclamationCircle
-                        className="h-5 w-5 text-red-500"
-                        aria-hidden="true"
-                      />
-                    </div>
-                  )}
-                </div>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+        <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
+          <form
+            noValidate={true}
+            className="space-y-6"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <div>
+              <Label htmlFor="email">Email</Label>
+              <div className="relative mt-2 rounded-md shadow-sm">
+                <Input
+                  {...register('email')}
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="you@example.com"
+                  variant={errors.email && 'error'}
+                />
                 {errors.email && (
-                  <p
-                    id="email-error"
-                    className="mt-2 text-sm text-red-600"
-                    role="alert"
-                  >
-                    {errors.email.message}
-                  </p>
+                  <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                    <HiExclamationCircle
+                      className="h-5 w-5 text-red-500"
+                      aria-hidden="true"
+                    />
+                  </div>
                 )}
               </div>
-
-              <div>
-                <Button
-                  type="submit"
-                  disabled={isSubmitting || searchParams}
-                  className="w-full leading-6"
+              {errors.email && (
+                <p
+                  id="email-error"
+                  className="mt-2 text-sm text-red-600"
+                  role="alert"
                 >
-                  Send link to email
-                </Button>
-              </div>
-            </form>
-          </div>
+                  {errors.email.message}
+                </p>
+              )}
+            </div>
+
+            <div>
+              <Button
+                type="submit"
+                disabled={isSubmitting || params}
+                className="w-full leading-6"
+              >
+                Send link to email
+              </Button>
+            </div>
+          </form>
         </div>
-      </Suspense>
+      </div>
     </>
   );
 }
